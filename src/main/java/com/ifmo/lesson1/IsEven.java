9b740f78da221a2e83638185ead1cc37735afe71
Kse-nia <rksenia07@mail.ru>
2021-10-13 20:24:04 +0300

    lesson1

---
+++ src/main/java/com/ifmo/lesson1/IsEven.java
@@ -0,0 +1,22 @@
+package com.ifmo.lesson1;
+
+public class IsEven {
+    /*
+    Напишите программу, которая выводит true, если число четное и false, если нет.
+    Попробуйте сделать при помощи битовых операций.
+     */
+    public static void main(String[] args) {
+        int n = 24;
+
+        System.out.println(isEven(n));
+    }
+
+    public static boolean isEven(int n) {
+        if (n % 2 == 0){
+            return true;
+        }
+
+// TODO implement
+        return false;
+    }
+}
